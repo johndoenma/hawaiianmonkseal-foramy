631a885fcbe06a461d53845fc28787c4540805e0
kccnma <gargiulo@hawaii.edu>
2024-04-01 14:08:45 -1000

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# hawaiianmonkseal-foramy
+a temp repo to give Amy site feedback
+
+[view site](https://johndoenma.github.io/hawaiianmonkseal-foramy)
---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
+  const menubutton = document.querySelector(".menu-button");
+  const menunav = document.querySelector(".toggle-nav");
+  menubutton.addEventListener("click", function () {
+    if (menunav.getAttribute("data-navstate") === "open") {
+      menunav.setAttribute("data-navstate", "closed");
+    } else {
+      menunav.setAttribute("data-navstate", "open");
+    }
+  });
+
+  // TOGGLE NAV FOR SMALL SCREEMS
+
+  var stickynavlinks = document.querySelectorAll(".sticky nav a");
+  var j;
+  for (j = 0; j < stickynavlinks.length; j++) {
+    stickynavlinks[j].onclick = function () {
+      menunav.setAttribute("data-navstate", "closed");
+    };
+  }
+});
